19df02d2a2d01584654c08b2adbf3e39625d2ada
foura1201 <84717712+foura1201@users.noreply.github.com>
2023-02-20 21:45:30 +0900

    validation 설정

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors(); // cors 활성화
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(3000);
 }
+
 bootstrap();
