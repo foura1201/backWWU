c118fae0c03df31a90cfe2507bf5e5a236ac21f1
unknown <acvxxx@naver.com>
2023-03-08 16:00:20 +0900

    Feat: get my info, change language

--- src/my/my.controller.ts
+++ src/my/my.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
   Put,
   Req,
@@ -17,10 +18,33 @@ import ServiceResult from 'src/lib/serviceResult';
 export class MyController {
   constructor(private myService: MyService) {}
 
+  @Get()
+  async getMyInfo(@Req() req, @Res() res: Response) {
+    const serviceResult: ServiceResult = await this.myService.getMyInfo(
+      req.user,
+    );
+    if (serviceResult.code === 200)
+      return res.status(200).json(serviceResult.data);
+    else return res.status(serviceResult.code).json(serviceResult.message);
+  }
+
   @Put()
   async modifyMyInfo(@Body() userDto, @Req() req, @Res() res: Response) {
     const serviceResult: ServiceResult = await this.myService.modifyMyInfo(
       userDto,
+      req.user,
+    );
+    return res.status(serviceResult.code).json(serviceResult.message);
+  }
+
+  @Put('language')
+  async changeLanguage(
+    @Body() preferredLanguage,
+    @Req() req,
+    @Res() res: Response,
+  ) {
+    const serviceResult: ServiceResult = await this.myService.changeLanguage(
+      preferredLanguage,
       req.user,
     );
     return res.status(serviceResult.code).json(serviceResult.message);
