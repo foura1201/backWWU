cc61b843b9c1c1e605468a3a51eed9cc6370b1f5
unknown <acvxxx@naver.com>
2023-03-14 15:12:37 +0900

    Feat: comment, postLike 기능

--- src/board/board.module.ts
+++ src/board/board.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
 import { TypeOrmExModule } from 'src/lib/typeorm-ex.module';
+import { CommentRepository } from 'src/repository/comment.repository';
+import { CommentReportRepository } from 'src/repository/commentReport.repository';
 import { PostRepository } from 'src/repository/post.repository';
+import { PostLikeRepository } from 'src/repository/postLike.repository';
+import { PostReportRepository } from 'src/repository/postReport.repository';
 import { RecruitRepository } from 'src/repository/recruit.repository';
+import { UserRepository } from 'src/repository/user.repository';
 import { BoardController } from './board.controller';
 import { BoardService } from './board.service';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([PostRepository]), AuthModule],
+  imports: [
+    TypeOrmExModule.forCustomRepository([PostRepository]),
+    TypeOrmExModule.forCustomRepository([CommentRepository]),
+    TypeOrmExModule.forCustomRepository([PostLikeRepository]),
+    TypeOrmExModule.forCustomRepository([PostReportRepository]),
+    TypeOrmExModule.forCustomRepository([UserRepository]),
+    TypeOrmExModule.forCustomRepository([CommentReportRepository]),
+    AuthModule,
+  ],
   controllers: [BoardController],
   providers: [BoardService],
 })
